4dc9b6018c259fa1a23f485c648702d6c477e89b
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2017-12-10 12:01:54 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// pull in express
+var express = require('express');
+var app = express();
+
+// link in these directories
+app.use(express.static(__dirname + '/view'));
+app.use(express.static(__dirname + '/script'));
+
+// send file to browser
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/view/index.html');
+});
+
+app.get('/iCV',function(req,res){
+  res.sendFile(__dirname + '/view/iCV.html');
+});
+
+app.get('/blog',function(req,res){
+  res.sendFile(__dirname + '/view/blog.html');
+});
+
+app.get('/apps',function(req,res){
+  res.sendFile(__dirname + '/view/apps.html');
+});
+
+app.get('/contact',function(req,res){
+  res.sendFile(__dirname + '/view/contact.html');
+});
+
+app.get('/submarine',function(req,res){
+  res.sendFile(__dirname + '/view/submarine.html');
+});
+
+// 0 is node binary
+// 1 is .js file
+// extra stuff starts from 2
+const port = parseInt(process.argv[2]) || 4000;
+
+// local port listen
+app.listen(port)
+
+console.log(`Running on port ${port}`)
